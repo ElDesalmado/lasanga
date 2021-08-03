aa0ae972c43b67761ac52280bd283e2f088a7fa0
s.kolesnik <7fosGbc9S>
2021-08-04 01:57:20 +0500

    implemented example in builder sandbox

--- sandbox.cpp
+++ sandbox.cpp
@@ -5,6 +5,135 @@
 #include <iostream>
 
 /**
+ * Given a builder-aware NonTemplate class with an N <= M nested members and a builder composed of
+ * M factories that are able to construct N nested members, the following is a valid expression.
+ *      auto builder = make_builder(wrap_factory<M>()...);
+ *      auto instance = make_lasanga<NonTemplate>(builder);
+ * Where `builder` is an object of generic template builder class specialized from M factories,
+ * `instance` is an object of user-defined NonTemplate class that uses `builder` to construct its
+ * nested members
+ *
+ * Given a builder-aware Template class with P template parameters (which may be template classes
+ * with J template parameters) and N <= M(P(J)) nested members and a builder composed of M(P(J))
+ * factories that are able to construct N nested members the following is a valid expression.
+ *      auto builder = make_builder(wrap_factory<P>()...
+ *                                  wrap_factory<J>()...);
+ *      auto instance = make_lasanga<Template>(builder);
+ * Where...
+ * `instance` is an object of deduced and fully specialized template class Template.
+ *
+ * In order to fully specialize template<A, B, C...>, where A, B and C may also be a
+ * template<A1, B1, C1...>
+ *
+ * Given a `template<template<typename...> class GenericClass, typename... Modifiers>`, function
+ * make_lasanga<GenericClass, Modifiers...> must:
+ *  - deduce the number of template parameters for `GenericClass` with a given set of `Modifiers`
+ *      - use a template_params_count<GenericClass, Modifiers...>
+ *          or
+ *      - count the number recursively, (?) using any_t as a template argument and checking
+ *
+ * // build by name
+ * template <typename>
+ * struct name_t{};
+ *
+ * // build by template name. Template NameTag can be used to overload a get<NameTag> function
+ * template <template <typename...> class>
+ * struct name_tt{};
+ *
+ * // TODO: (?) generic template tag to distinguish between template and template template
+ *
+ * // Dependent Name Tag. build by name within a template GenericClass using a set of Modifiers
+ * template <template <typename...> class GenericClass, typename NameTag, typename ... Modifiers>
+ * struct dname_t{};
+ *
+ * // build by type. May be used only if Type is already specialized
+ * template <typename>
+ * struct build_t{};
+ *
+ * template <template <typename...> class>
+ * struct build_tt{};
+ *
+ * struct NameA;
+ * struct NameB;
+ * struct NameC;
+ *
+ * template <typename A, typename B, typename C>
+ * class Layer
+ * {
+ * public:
+ *   template <typename BuilderT>
+ *   Layer(BuilderT &&builder)
+ *      : a_(builder(name_t<NameA>()), // A must be deduced from Builder in make_layer, only by
+ * NameA tag
+ *        b_(builder(dname_t<Layer, NameB>()), // B must be deduced from Builder in make_layer.
+ * Builder will use Layer and NameB tag to deduce B if specified, otherwise will use just NameB tag
+ *        c_(builder(name_t<NameC>()), // C must be deduced from Builder in make_layer, only by
+ * NameC tag
+ *        nt_(builder(build_t<NonTemplate>()) // does not need to be deduced, Builder must contain a
+ * designated factory
+ *   {}
+ * private:
+ *      A a_;
+ *      B b_;
+ *      C c_;
+ *      NonTemplate nt_;
+ * };
+ *
+ * template <template<typename, typename, typename> class LayerT, typename BuilderT>
+ * constexpr auto make_layer(BuilderT &&builder)
+ * {
+ *      // builder
+ *      // Can this be automated?
+ *      using spec_a =
+ *          typename traits::builder<std::decay_t<BuilderT>>::template name_type<NameA>::type;
+ *      using spec_b =
+ *          typename traits::builder<std::decay_t<BuilderT>>::template name_type<NameB>::type;
+ *      using spec_c =
+ *          typename traits::builder<std::decay_t<BuilderT>>::template name_type<NameC>::type;
+ *
+ *      return Layer<spec_a, spec_b, spec_c>(builder);
+ * }
+ *
+ * When making a builder, user
+ */
+
+
+template<template<typename...> class>
+struct tag_tt
+{
+};
+
+/*
+ * Is one?
+ * Is Infinite (max available)
+ */
+
+template<template<typename> class GenericClass>
+constexpr size_t template_params_count(tag_tt<GenericClass>)
+{
+    return 1;
+}
+
+template<template<typename, typename> class GenericClass>
+constexpr size_t template_params_count(tag_tt<GenericClass>)
+{
+    return 2;
+}
+
+template<typename>
+class SingleParam
+{
+};
+
+template<typename, typename>
+class DoubleParams
+{
+};
+
+static_assert(template_params_count(tag_tt<SingleParam>()) == 1);
+static_assert(template_params_count(tag_tt<DoubleParams>()) == 2);
+
+/**
  * auto builder = make_builder(...
  *      algo<generic_class<ImplA>>,
  *      // algo<generic_class<ImplB>>
---
+++ sb_builder.cpp
@@ -0,0 +1,199 @@
+﻿
+#include "lasanga/builder.h"
+
+#include <utility>
+
+template<typename...>
+struct type_list;
+
+template<template<typename...> class /*GenericClass*/, typename /*TypeList*/>
+struct specialize_t;
+
+template<template<typename...> class GenericClass, typename... Types>
+struct specialize_t<GenericClass, type_list<Types...>>
+{
+    using type = GenericClass<Types...>;
+};
+
+// TODO: use builder traits (?)
+template<typename BuilderT, typename NameTag>
+using type_by_name = typename BuilderT::template type_by_name<NameTag>::type;
+
+template<typename /*BuilderT*/, typename /*NameList*/>
+struct get_type_list;
+
+template<typename BuilderT, typename... NameTags>
+struct get_type_list<BuilderT, type_list<NameTags...>>
+{
+    using type = type_list<type_by_name<BuilderT, NameTags>...>;
+};
+
+template<template<typename...> class GenericClass, typename... Modifiers>
+struct get_name_list;
+
+template<template<typename...> class GenericClass,
+         template<template<typename...> class, typename...> class GetNameList = get_name_list,
+         typename... Modifiers,
+         typename BuilderT>
+constexpr auto make_lasanga(BuilderT &&builder)
+{
+    using name_list = typename GetNameList<GenericClass, Modifiers...>::type;
+
+    // TODO: get list of types from builder
+    using type_list_t = typename get_type_list<std::decay_t<BuilderT>, name_list>::type;
+
+    return typename specialize_t<GenericClass, type_list_t>::type(builder);
+}
+
+// build by NameTag
+template<typename>
+struct name_t
+{
+};
+
+// build by template NameTag. Template NameTag can be used to overload a get<NameTag> function
+template<template<typename...> class>
+struct name_tt
+{
+};
+
+// build by dependent NameTag and a set of Modifiers. NameTag of a type used within a template
+// GenericClass
+template<template<typename...> class GenericClass, typename NameTag, typename... Modifiers>
+struct dname_t
+{
+};
+
+template<typename>
+struct build_t
+{
+};
+
+template<template<typename...> class NameTag, typename... Modifiers>
+struct build_tt
+{
+};
+
+///////// Example
+
+#include <iostream>
+
+struct NonTemplate
+{
+    void speak() { std::cout << "NonTemplate" << std::endl; }
+};
+
+// This part should be defined in user's generic/layer.h header
+
+namespace name_tags
+{
+    struct A;
+
+    // TODO: how to use? type_list<B> will not work
+    //    template<typename...>
+    //    struct B;
+
+    struct B;
+
+    struct C;
+}   // namespace name_tags
+
+template<typename A, typename B, typename C>
+class Layer
+{
+public:
+    template<typename BuilderT>
+    Layer(BuilderT &&builder)
+      : a_(builder(name_t<name_tags::A>())),
+        //        b_(builder(name_tt<name_tags::B>())), // TODO: how to work with this? type_list<B>
+        // will not compile
+        b_(builder(name_t<name_tags::B>())),
+        c_(builder(dname_t<Layer, name_tags::C>())),
+        nt_(builder(build_t<NonTemplate>()))
+    {
+    }
+
+    void speak_all()
+    {
+        a_.speak();
+        b_.speak();
+        c_.speak();
+        nt_.speak();
+    }
+
+private:
+    A a_;
+    B b_;
+    C c_;
+    NonTemplate nt_;
+};
+
+template<>
+struct get_name_list<Layer>
+{
+    using type = type_list<name_tags::A, name_tags::B, name_tags::C>;
+};
+
+// end of user-defined header
+
+struct Person
+{
+    void speak() { std::cout << "Person" << std::endl; }
+};
+
+struct Dog
+{
+    void speak() { std::cout << "Dog" << std::endl; }
+};
+
+struct Cat
+{
+    void speak() { std::cout << "Cat" << std::endl; }
+};
+
+// TODO: create Builder using make_builder function
+// TODO: make builder using name tags and designated factories
+struct Builder
+{
+    // these operators must be generated automatically
+
+    NonTemplate operator()(build_t<NonTemplate>) { return {}; }
+
+    Person operator()(name_t<name_tags::A>) { return {}; }
+
+    Dog operator()(name_t<name_tags::B>) { return {}; }
+
+    Cat operator()(name_t<name_tags::C>) { return {}; }
+
+    // if this is not declared within the Builder, name_t must be used
+    Cat operator()(dname_t<Layer, name_tags::C>) { return {}; }
+
+    template<typename NameTag>
+    struct type_by_name;
+};
+
+// This must be automatically generated for the Builder in case NameTag was bound
+template<>
+struct Builder::type_by_name<name_tags::A>
+{
+    using type = Person;
+};
+template<>
+struct Builder::type_by_name<name_tags::B>
+{
+    using type = Dog;
+};
+template<>
+struct Builder::type_by_name<name_tags::C>
+{
+    using type = Cat;
+};
+
+int main(int, char **)
+{
+    auto deducedLayer = make_lasanga<Layer>(Builder());
+    deducedLayer.speak_all();
+
+    //
+    return 0;
+}
