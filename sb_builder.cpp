911e2270ab7ac08ef041620baab07891a5ba3554
s.kolesnik <7fosGbc9S>
2021-08-16 23:00:57 +0500

    implemented map_tuple in sandbox

--- sb_builder.cpp
+++ sb_builder.cpp
@@ -87,9 +87,200 @@ struct create_cat
 
 Person create_person() { return {}; }
 
+namespace detail
+{
+    template<typename WrappedPredicateT, typename T>
+    struct apply_predicate;
+
+    template<template<typename> class Predicate,
+             typename T,
+             template<template<typename> class>
+             class PredicateWrapper>
+    struct apply_predicate<PredicateWrapper<Predicate>, T>
+    {
+        constexpr static auto value = Predicate<T>::value;
+    };
+
+    template<typename T, typename... WrappedPredicates>
+    struct apply_predicates
+    {
+        constexpr static auto value = std::conjunction_v<apply_predicate<WrappedPredicates, T>...>;
+    };
+
+    template<typename TupleT,
+             typename ListWrappedPredicatesT,
+             typename = decltype(std::make_index_sequence<std::tuple_size_v<TupleT>>())>
+    struct map_tuple_index_sequence;
+
+    template<template<typename...> class TupleT,
+             typename... Types,
+             template<typename...>
+             class TypeListT,
+             typename... WrappedPredicatesT,
+             size_t... Indx>
+    struct map_tuple_index_sequence<TupleT<Types...>,
+                                    TypeListT<WrappedPredicatesT...>,
+                                    std::index_sequence<Indx...>>
+    {
+    private:
+        using tuple_input = TupleT<Types...>;
+
+        using tuple_indices = decltype(std::tuple_cat(
+            std::conditional_t<apply_predicates<Types, WrappedPredicatesT...>::value,
+                               std::tuple<std::integral_constant<size_t, Indx>>,
+                               std::tuple<>>()...));
+
+        template<typename TupleI>
+        struct sequence_from_tuple;
+
+        template<template<typename...> class TupleIC, size_t... IC>
+        struct sequence_from_tuple<TupleIC<std::integral_constant<size_t, IC>...>>
+        {
+            using type = std::index_sequence<IC...>;
+        };
+
+    public:
+        using type = typename sequence_from_tuple<tuple_indices>::type;
+    };
+
+    template<typename TupleT,
+             typename ListWrappedPredicatesT,
+             typename = typename map_tuple_index_sequence<TupleT, ListWrappedPredicatesT>::type>
+    struct map_tuple;
+
+    template<template<typename...> class TupleT,
+             typename... T,
+             typename ListWrappedPredicatesT,
+             size_t... Indx>
+    struct map_tuple<TupleT<T...>, ListWrappedPredicatesT, std::index_sequence<Indx...>>
+    {
+        constexpr auto operator()(TupleT<T...> &tuple)
+        {
+            return TupleT<std::tuple_element_t<Indx, std::decay_t<decltype(tuple)>> &...>(
+                std::get<Indx>(tuple)...);
+        }
+
+        constexpr auto operator()(const TupleT<T...> &tuple)
+        {
+            return TupleT<std::tuple_element_t<Indx, std::decay_t<decltype(tuple)>> &...>(
+                std::get<Indx>(tuple)...);
+        }
+    };
+
+    template<typename...>
+    struct type_list;
+
+}   // namespace detail
+
+template<template<typename> class>
+struct wrapped_predicate;
+
+template<typename... WrappedPredicates, typename TupleT>
+constexpr auto map_tuple(TupleT &&tuple)
+{
+    return detail::map_tuple<std::decay_t<TupleT>, detail::type_list<WrappedPredicates...>>{}(
+        tuple);
+}
+
+template<typename... DesignatedFactories>
+class builder
+{
+public:
+    /**
+     * Building:
+     * One tag type (build, name, d_name) specialization allows construction of only one type of
+     * object.
+     * - using build_t: will build an object of typename
+     * - using name_t and name_tt: will build an object that is registered with the given alias
+     * - using d_name_t and d_name_tt: will try to build an object using a dependent name. If no
+     * dependent name is registered, will fall back to name_t or name_tt
+     *
+     * Type tree for specialization:
+     * Given a root template class, builder will use root's name_list to recursively create a type
+     * tree that can be used to fully specialize the given template class.
+     */
+
+    /**
+     * Build using build_t tag.
+     * \details First will try to find a unique unnamed designated factory.
+     * Then will try to find a unique named designated factory.
+     * If other than one factories are suitable, will throw a compile time error.
+     * @tparam T
+     * @return
+     */
+    template<typename T>
+    decltype(auto) operator()(eld::build_t<T> buildTag)
+    {
+        // TODO: get tuple of unnamed factories
+        auto tuple = std::tuple<>{};
+        return construct(buildTag, tuple, std::is_same<decltype(tuple), std::tuple<>>());
+    }
+
+    template<typename NameTag>
+    decltype(auto) operator()(eld::name_t<NameTag>)
+    {
+        // TODO: get tuple of named factories
+        auto tuple = std::tuple<>{};
+        return construct(tuple);
+    }
+
+private:
+    template<typename T, typename... Factories>
+    decltype(auto) construct(eld::build_t<T>,
+                             std::tuple<Factories &...> tupleFactories,
+                             std::false_type /*is_empty*/)
+    {
+        return construct(tupleFactories);
+    }
+
+    template<typename T, typename... Factories>
+    decltype(auto) construct(eld::build_t<T>,
+                             std::tuple<Factories &...> tupleFactories,
+                             std::true_type /*is_empty*/)
+    {
+        // TODO: find a type T within named factories
+        auto tuple = std::tuple<>{};
+        return construct(tuple);
+    }
+
+    template<typename... FoundFactories>
+    decltype(auto) construct(std::tuple<FoundFactories &...> factories)
+    {
+        constexpr size_t tupleSize = std::tuple_size_v<decltype(factories)>;
+        static_assert(tupleSize != 0, "No designated factories have been found.");
+        static_assert(
+            tupleSize <= 1,
+            "Can't resolve designated factory. Several suitable factories were registered");
+
+        return std::get<0>(factories)();
+    }
+
+private:
+    std::tuple<DesignatedFactories...> designatedFactories_;
+};
+
 int main(int, char **)
 {
     namespace detail = eld::detail;
+
+    struct dummy
+    {
+    };
+
+    using tuple_input = std::tuple<double, int, dummy, size_t>;
+
+    using output_sequence = typename ::detail::map_tuple_index_sequence<
+        tuple_input,
+        ::detail::type_list<wrapped_predicate<std::is_integral>>>::type;
+
+    static_assert(std::is_same_v<output_sequence, std::index_sequence<1, 3>>);
+
+    tuple_input tupleInput{};
+    auto mappedTuple = map_tuple<wrapped_predicate<std::is_integral>>(tupleInput);
+    static_assert(std::is_same_v<decltype(mappedTuple), std::tuple<int &, size_t &>>);
+
+    auto mappedTuple2 = map_tuple<wrapped_predicate<std::is_integral>>(tuple_input());
+    static_assert(std::is_same_v<decltype(mappedTuple2), std::tuple<int &, size_t &>>);
 
     eld::wrap_factory (&create_person)(eld::build_tag<Person>()).speak();
     eld::wrap_factory([]() { return Dog(); })(eld::build_tag<Dog>()).speak();
