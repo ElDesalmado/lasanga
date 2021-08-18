aef569c5680251156e02d0fb489e69f7243d268d
s.kolesnik <7fosGbc9S>
2021-08-18 13:25:34 +0500

    compiles with one member construction

--- sb_builder.cpp
+++ sb_builder.cpp
@@ -28,36 +28,39 @@ namespace alias
 
 // TODO: function build(Builder, tag<NameOrType>) to deduce return type? (Object or Tuple)
 
-template<typename A, typename B, typename C>
-class Layer
+namespace space
 {
-public:
-    template<typename BuilderT>
-    Layer(BuilderT &&builder)
-      : a_(builder(eld::name_tag<alias::A>())),
-        b_(builder(eld::name_tag<alias::B>())),
-        c_(builder(eld::d_name_tag<alias::C, ::Layer>())),
-        nt_(builder(eld::build_tag<NonTemplate>()))
+    template<typename A, typename B, typename C>
+    class Layer
     {
-    }
+    public:
+        template<typename BuilderT>
+        Layer(BuilderT &&builder)
+          : a_(builder(eld::name_tag<alias::A>())),
+            b_(builder(eld::name_tag<alias::B>())),
+            c_(builder(eld::d_name_tag<alias::C, space::Layer>())),
+            nt_(builder(eld::build_tag<NonTemplate>()))
+        {
+        }
 
-    void speak_all()
-    {
-        a_.speak();
-        b_.speak();
-        c_.speak();
-        nt_.speak();
-    }
+        void speak_all()
+        {
+            a_.speak();
+            b_.speak();
+            c_.speak();
+            nt_.speak();
+        }
 
-private:
-    A a_;
-    B b_;
-    C c_;
-    NonTemplate nt_;
-};
+    private:
+        A a_;
+        B b_;
+        C c_;
+        NonTemplate nt_;
+    };
+}   // namespace space
 
 template<>
-struct eld::get_name_list<Layer>
+struct eld::get_name_list<space::Layer>
 {
     using type = type_list<alias::A, type_tt<alias::B>, alias::C>;
 };
@@ -148,12 +151,12 @@ int main(int, char **)
     eld::d_named_factory<alias::B, alias::B, create_cat>()().speak();
     eld::d_named_factory<alias::B, alias::B> (&create_person)().speak();
 
-    eld::d_named_factory<alias::C, Layer, create_cat>()().speak();
+    eld::d_named_factory<alias::C, space::Layer, create_cat>()().speak();
 
     auto builder =   // TODO: make it work
         eld::make_builder(eld::named_factory<alias::A>(&create_person),
                           eld::named_factory<alias::B>([]() { return Dog(); }),
-                          eld::d_named_factory<alias::C, Layer, create_cat>(),
+                          eld::d_named_factory<alias::C, space::Layer, create_cat>(),
                           eld::wrap_factory([]() { return NonTemplate(); }));
 
     builder(eld::build_tag<Person>()).speak();
@@ -163,9 +166,9 @@ int main(int, char **)
     builder(eld::name_tag<alias::A>()).speak();
     builder(eld::name_tag<alias::B>()).speak();
     builder(eld::name_tag<alias::C>()).speak();
-    builder(eld::d_name_tag<alias::C, Layer>()).speak();
+    builder(eld::d_name_tag<alias::C, space::Layer>()).speak();
 
-    auto deducedLayer = eld::make_lasanga<Layer>(builder);
+    auto deducedLayer = eld::make_lasanga<space::Layer>(builder);
     deducedLayer.speak_all();
 
     return 0;
